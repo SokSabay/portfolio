11f723f009bec31b2c90c2c962e3d0821a1d9296
SokSabay <baptiste.chenot@gmail.com>
2022-02-24 11:30:56 +0100

    change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Project from "./pages/Project";
@@ -8,13 +8,13 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route exact="true" path="/" element={<Home />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route exact="true" path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </HashRouter>
   );
 }
 export default App;
